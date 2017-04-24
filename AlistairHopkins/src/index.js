a181fd22aac823d894327f70eb287ec93ade4983
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-04-24 13:56:30 +0100

    setting up of the routing

--- AlistairHopkins/src/index.js
+++ AlistairHopkins/src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Match, Miss } from 'react-router';
+import Main from "./components/Main"
 
 const Root = () => {
     return (
         <BrowserRouter>
             <div>
-                <Match exactly pattern="/" component={StorePicker}></Match>
-                <Match pattern="/store/:storeId" component={App}></Match>
-                <Miss component={NotFound} />
+                <Match exactly pattern="/" component={Main}></Match>
             </div>
         </BrowserRouter>
     )
