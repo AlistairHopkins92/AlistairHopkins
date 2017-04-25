07a0d1642046a5353e73cd711804ac0bd248586e
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-04-25 17:56:41 +0100

    updating the stack page

--- AlistairHopkins/src/components/Stack.js
+++ AlistairHopkins/src/components/Stack.js
@@ -13,20 +13,44 @@ class Stack extends React.Component {
                 <div className="stack">
                     <div className="stack-column">
                         <h4>Past</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
-                        <h4>Angular</h4>
+                        <p> 
+                            Angular Ionic/Cordova
+                            AWS Lambda
+                            AWS API Gateway
+                            Jira
+                            Twilio Stripe
+                            Heroku Now.sh
+                            Docker Ubuntu
+                            Github
+                            Gulp Bower
+                            Firebase
+                        </p>
                     </div>
                     <div className="stack-column">
                         <h4>Present</h4>
-
+                        <p> 
+                            React
+                            React Redux ES6
+                            Digital Ocean
+                            Ruby on Rails
+                            Node Express NPM
+                            Flexbox CSSModules
+                            Vagrant VSCode
+                            Postgresql Webpack
+                            Git Gitlab
+                            Google Maps API
+                            Postman CircleCI
+                            Droplr Trello
+                        </p>
                     </div>
                     <div className="stack-column">
                         <h4>Future</h4>
+                        <p> 
+                            React Native
+                            D3 Centos Nginx
+                            Enzyme
+                            Toolbox
+                        </p>
                     </div>
                 </div>
             </div>
