47c8d5845299975c3916acfcc5ed40e017d393b2
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-05-03 20:51:13 +0100

    rendering from data file instead of all in the html

--- AlistairHopkins/src/components/Stack.js
+++ AlistairHopkins/src/components/Stack.js
@@ -16,9 +16,16 @@ class Stack extends React.Component {
         const futureStack = stack.filter(function(item, index, array){
             return item.tense === "future"
         })
-        console.log(pastStack)
-        console.log(currentStack)
-        console.log(futureStack)
+
+        const pastListings = pastStack.map((item) =>
+            <li>{item.name}</li>
+        );
+        const currentListings = currentStack.map((item) =>
+            <li>{item.name}</li>
+        );
+        const futureListings = futureStack.map((item) =>
+            <li>{item.name}</li>
+        );
 
         return (
             <div>
@@ -29,43 +36,20 @@ class Stack extends React.Component {
                     <div className="stack-column">
                         <h1>Past</h1>
                         <ul> 
-                            <li>Angular Ionic/Cordova</li>
-                            <li>AWS API Gateway</li>
-                            <li>Jira</li>
-                            <li>Twilio Stripe</li>
-                            <li>Heroku</li>
-                            <li>Now.sh</li>
-                            <li>Github</li>
-                            <li>Gulp</li>
-                            <li>Bower</li>
-                            <li>Firebase</li>
+                            {pastListings}
                         </ul>
                     </div>
                     <div className="stack-column">
-                        <h1>Present</h1>
-                        <p> 
-                            React
-                            React Redux ES6
-                            Digital Ocean
-                            Ruby on Rails
-                            Node Express NPM
-                            Flexbox CSSModules
-                            Vagrant VSCode
-                            Postgresql Webpack
-                            Git Gitlab
-                            Google Maps API
-                            Postman CircleCI
-                            Droplr Trello
-                        </p>
+                        <h1>Current</h1>
+                        <ul> 
+                            {currentListings}
+                        </ul>
                     </div>
                     <div className="stack-column">
                         <h1>Future</h1>
-                        <p> 
-                            React Native
-                            D3 Centos Nginx
-                            Enzyme
-                            Toolbox
-                        </p>
+                        <ul> 
+                            {futureListings}
+                        </ul>
                     </div>
                 </div>
             </div>
