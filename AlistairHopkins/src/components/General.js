9b69e85442a92628d41bbf94107c5e9c705c58f0
AlistairHopkins92 <alistairhopkins92@gmail.com>
2017-05-08 19:30:09 +0100

    last bit for the night. Some wording left to play with but site is done

--- AlistairHopkins/src/components/General.js
+++ AlistairHopkins/src/components/General.js
@@ -19,7 +19,7 @@ class General extends React.Component {
 
                                 <li><i className="devicon-html5-plain colored"></i></li>
                                 <li><i className="devicon-javascript-plain colored"></i></li>
-                                <li><i className="devicon-jquery-plain-wordmark colored"></i></li>
+                                <li><i className="devicon-jquery-plain-wordmark"></i></li>
                                 <li><i className="devicon-nodejs-plain colored"></i></li>
 
                                 <li><i className="devicon-angularjs-plain colored"></i></li>
@@ -30,12 +30,12 @@ class General extends React.Component {
                                 <li><i className="devicon-css3-plain-wordmark"></i></li>
                                 <li><i className="devicon-sass-original colored"></i></li>
                                 <li><i className="devicon-bootstrap-plain-wordmark colored"></i></li>
-                                <li><i className="devicon-foundation-plain-wordmark"></i></li>
+                                <li><i className="devicon-grunt-plain-wordmark colored"></i></li>
 
-                                <li><i className="devicon-protractor-plain colored"></i></li>
+                                <li><i className="devicon-gulp-plain colored"></i></li>
                                 <li><i className="devicon-mocha-plain colored"></i></li>
                                 <li><i className="devicon-jasmine-plain-wordmark colored"></i></li>
-                                <li><i className="devicon-trello-plain colored"></i></li>
+                                <li><i className="devicon-protractor-plain colored"></i></li>
                                 
                             </ul>
                         </div>
@@ -46,7 +46,8 @@ class General extends React.Component {
                             role="presentation"
                             />*/}
                             <p>Originating from the North of England, I'm a Web Developer with a keen interest in understanding how to make things work. I recently finished my first software development role after graduating an intensive web development course at General assembly. I’m now looking for a development position where I can get heavily involved with new technologies as a part of a dynamic team, and also build upon my existing skills and knowledge.</p>
-                            <p>Since finishing my contract at Roco Partners I’ve split my time between working on some freelance projects and improving my skill set by learning React and Redux. One of the freelance projects involved looking at a WordPress website for a tool resale company, focusing mainly on speed optimisation as well as fixing some bugs and looking at potential new features.</p>
+                            <p>Since finishing my contract at Roco Partners I’ve split my time between working on some freelance projects and improving my skill set by learning React and Redux. </p>
+                            <p>One of the freelance projects involved looking at a WordPress website for a tool resale company, focusing mainly on speed optimisation as well as fixing some bugs and looking at potential new features.</p>
                         </div>
                     </div>
                 </div>
